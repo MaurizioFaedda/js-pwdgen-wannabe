52396910ce9c49d73812aa2ae9e2aa81e9d0f5ae
Maurizio <maurizio.faedda1@gmail.com>
2020-10-12 16:01:36 +0200

    Esempio struttura algoritmo completata

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nomeUtente = prompt("Ciao, come ti chiami?");
+
+var cognomeUtente = prompt("Grazie " + nomeUtente + ", ora inserisci il tuo cognome:");
+
+var colore = prompt("Bene " + nomeUtente + " " + cognomeUtente + ", per finire inserisci il tuo colore preferito e la tua super password sarà generata!");
+
+document.getElementById("nome").innerHTML =
+nomeUtente + cognomeUtente + colore + "20";
